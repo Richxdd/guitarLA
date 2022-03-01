cb772e2b94c347878584f702da64a34339c1e7fb
RICARDO SOLIS <ricardosv46@gmail.com>
2022-03-01 01:53:50 -0500

    update

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,29 +1,26 @@
+import Layout from "../components/Layout"
+import ListadoBlog from "../components/ListadoBlog"
 
-
-import Layout from '../components/Layout'
-import ListadoBlog from '../components/ListadoBlog'
-
-const Blog = ({entradas}) => {
-
-    return (
-        <Layout pagina='Blog'>
-          <main className='contenedor'>
-           <ListadoBlog entradas={entradas}/>
-          </main>
-        </Layout>
-    )
+const Blog = ({ entradas }) => {
+  console.log(entradas)
+  return (
+    <Layout pagina='Blog'>
+      <main className='contenedor'>
+        <ListadoBlog entradas={entradas} />
+      </main>
+    </Layout>
+  )
 }
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/blogs`
+  const respuesta = await fetch(url)
+  const entradas = await respuesta.json()
 
-        const url =`${process.env.API_URL}/blogs`
-        const respuesta = await fetch(url)
-        const entradas = await respuesta.json()
-
-  return{
-    props:{
-      entradas
-    }
+  return {
+    props: {
+      entradas,
+    },
   }
 }
 
--- pages/blog/[url].js
+++ pages/blog/[url].js
@@ -1,61 +1,60 @@
-
-import { formatearFecha } from "../../helpers/Index";
-import Image from "next/image";
-import Layout from "../../components/Layout";
-import styles from '../../styles/Entrada.module.css';
-
-
+import { formatearFecha } from "../../helpers/Index"
+import Image from "next/image"
+import Layout from "../../components/Layout"
+import styles from "../../styles/Entrada.module.css"
 
 const EntradaBlog = ({ entrada }) => {
-  
-    const { titulo, contenido, imagen, published_at } = entrada[0]
-    
-    return (
-        <Layout pagina={titulo}>
-        <main className="contenedor">    
-                <h1 className="heading">{titulo}</h1>
-                <article className={styles.entrada}>
-                    <Image width={800} height={600} priority='true' layout="responsive" src={imagen.url} alt={`imagen blog ${titulo}`}/>
-                    <div className={styles.contenido}>
-                        <p className={styles.fecha}>{formatearFecha(published_at)}</p>
-                    <p className={styles.texto}>{contenido}</p>
-                    </div>
-                </article>
-        </main>
-        </Layout>
-    )
+  const { titulo, contenido, imagen, published_at } = entrada[0]
+
+  return (
+    <Layout pagina={titulo}>
+      <main className='contenedor'>
+        <h1 className='heading'>{titulo}</h1>
+        <article className={styles.entrada}>
+          <Image
+            width={800}
+            height={600}
+            priority='true'
+            layout='responsive'
+            src={imagen.url}
+            alt={`imagen blog ${titulo}`}
+          />
+          <div className={styles.contenido}>
+            <p className={styles.fecha}>{formatearFecha(published_at)}</p>
+            <p className={styles.texto}>{contenido}</p>
+          </div>
+        </article>
+      </main>
+    </Layout>
+  )
 }
 
-export async function getStaticPaths(){
-    const url = `${process.env.API_URL}/blogs`
-    const respuesta = await fetch(url)
-    const entrada = await respuesta.json()
+export async function getStaticPaths() {
+  const url = `${process.env.API_URL}/blogs`
+  const respuesta = await fetch(url)
+  const entrada = await respuesta.json()
 
-    const paths = entrada.map(entrada=>({
-        params:{url: entrada.url}
-    }))
+  const paths = entrada.map((entrada) => ({
+    params: { url: entrada.url },
+  }))
 
-    console.log(paths);
-    return{
-        paths,
-        fallback: false
-    }
-
+  console.log(paths)
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export async function getStaticProps({ params: { url } }) {
+  const urls = ` ${process.env.API_URL}/blogs?url=${url}`
+  const respuesta = await fetch(urls)
+  const entrada = await respuesta.json()
 
-    const urls =` ${process.env.API_URL}/blogs?url=${url}`
-    const respuesta = await fetch(urls)
-    const entrada = await respuesta.json()
-
-
-    return {
-        props: {
-            entrada
-           
-        }
-    }
+  return {
+    props: {
+      entrada,
+    },
+  }
 }
 
 // export async function getServerSideProps({ query: { url } }) {
@@ -67,7 +66,7 @@ export async function getStaticProps({ params: { url } }) {
 //     return {
 //         props: {
 //             entrada
-           
+
 //         }
 //     }
 // }
