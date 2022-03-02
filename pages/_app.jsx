740ca32b6461bf0da0bc94e5bfb5a3990f4e7046
RICARDO SOLIS <ricardosv46@gmail.com>
2022-03-01 21:58:10 -0500

    feat:añadiendo carrito de compras

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,56 @@
+import { useState, useEffect } from "react"
+import "../styles/normalize.css"
+import "../styles/globals.css"
 
-import '../styles/normalize.css'
-import '../styles/globals.css'
+const MyApp = ({ Component, pageProps }) => {
+  const [carrito, setCarrito] = useState([])
 
-const MyApp=({ Component, pageProps })=> {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const carritoLs = JSON.parse(localStorage.getItem("carrito")) ?? []
+    setCarrito(carritoLs)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+  }, [carrito])
+
+  const agregarCarrito = (producto) => {
+    if (carrito.some((articulo) => articulo.id === producto.id)) {
+      const carritoActualizado = carrito.map((articulo) => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad
+        }
+        return articulo
+      })
+
+      setCarrito(carritoActualizado)
+    } else {
+      setCarrito([...carrito, producto])
+    }
+  }
+
+  const actualizarCantidad = (producto) => {
+    const carritoActualizado = carrito.map((articulo) => {
+      if (articulo.id === producto.id) {
+        articulo.cantidad = producto.cantidad
+      }
+      return articulo
+    })
+    setCarrito(carritoActualizado)
+  }
+  const eliminarProducto = (id) => {
+    const carritoActualizado = carrito.filter((articulo) => articulo.id !== id)
+    setCarrito(carritoActualizado)
+  }
+  return (
+    <Component
+      {...pageProps}
+      carrito={carrito}
+      agregarCarrito={agregarCarrito}
+      actualizarCantidad={actualizarCantidad}
+      eliminarProducto={eliminarProducto}
+    />
+  )
 }
 
 export default MyApp
